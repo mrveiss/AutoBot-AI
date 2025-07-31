ce28de5f241ba2d5f5be89bd5fec3c198e6edf39
mrveiss <martins.veiss@gmail.com>
2025-07-31 17:21:06 +0300

    consolidate chat history management and tool registry

---
+++ src/tools/__init__.py
@@ -0,0 +1,10 @@
+"""
+Unified Tools Package
+
+This package provides a centralized tool registry that eliminates duplication
+between the standard orchestrator and LangChain orchestrator implementations.
+"""
+
+from .tool_registry import ToolRegistry
+
+__all__ = ['ToolRegistry']
