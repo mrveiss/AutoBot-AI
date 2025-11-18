40134cd91b18a3c3367c827f9819d3ff1ed3522d
Martins Veiss <martins.veiss@gmail.com>
2025-11-18 12:32:03 +0200

    refactor(config): Migrate src/utils files to unified_config (#63)
    - Migrate 6 utility files:
      - hybrid_search.py
      - knowledge_cache.py
      - model_optimizer.py
      - monitoring_alerts.py
      - performance_monitor.py
      - service_registry.py
    - Replace all config_helper with unified_config
    - Part of Phase 3 config consolidation
    - 16 of 24 files migrated

--- src/utils/monitoring_alerts.py
+++ src/utils/monitoring_alerts.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 from enum import Enum
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
-from src.config_helper import cfg
+from src.unified_config import config
 from src.constants.network_constants import NetworkConstants
 
 logger = logging.getLogger(__name__)
