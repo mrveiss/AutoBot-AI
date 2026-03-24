c898966da437d57db5e4ca9ae3875e32512b3844
Martins Veiss <martins.veiss@gmail.com>
2026-03-24 21:42:16 +0200

    cleanup(analytics): remove dead barrel exports for deleted components (#2262)

--- autobot-frontend/src/components/analytics/index.ts
+++ autobot-frontend/src/components/analytics/index.ts
@@ -13,8 +13,6 @@
 export { default as AnalyticsHeader } from './AnalyticsHeader.vue'
 export { default as IndexingProgress } from './IndexingProgress.vue'
 export { default as EnhancedAnalyticsGrid } from './EnhancedAnalyticsGrid.vue'
-export { default as CodebaseStatsSection } from './CodebaseStatsSection.vue'
-export { default as ProblemsReportSection } from './ProblemsReportSection.vue'
 export { default as DuplicatesSection } from './DuplicatesSection.vue'
 export { default as DeclarationsSection } from './DeclarationsSection.vue'
 export { default as CodeSmellsSection } from './CodeSmellsSection.vue'
