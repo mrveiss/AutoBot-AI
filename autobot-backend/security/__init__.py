b2214d0df269b3de638c3656dbf342f94edcee7e
mrveiss <martins.veiss@gmail.com>
2026-04-12 13:20:06 +0300

    WIP: preserve work from issue-3394

--- autobot-backend/security/__init__.py
+++ autobot-backend/security/__init__.py
@@ -6,6 +6,17 @@ Security module for AutoBot
 Provides service-to-service authentication and authorization
 """
 
+from middleware.service_auth_enforcement import (
+    get_endpoint_categories,
+    get_enforcement_mode,
+    log_enforcement_status,
+)
 from security.service_auth import ServiceAuthManager, validate_service_auth
 
-__all__ = ["ServiceAuthManager", "validate_service_auth"]
+__all__ = [
+    "ServiceAuthManager",
+    "validate_service_auth",
+    "get_enforcement_mode",
+    "log_enforcement_status",
+    "get_endpoint_categories",
+]
