b07bbde0598a712239da97a14fe3342de964c8ad
Martins Veiss <martins.veiss@gmail.com>
2026-04-14 10:31:51 +0300

    fix(workers): add __all__ exports to new worker modules (#4322) (#4477)
    Add __all__ exports to autobot-npu-worker and autobot-browser-worker modules
    to clearly document public APIs per Python best practices.

--- autobot-browser-worker/src/__init__.py
+++ autobot-browser-worker/src/__init__.py
@@ -2,3 +2,7 @@
 # Copyright (c) 2025 mrveiss
 # Author: mrveiss
 """Browser Worker Source Code Package"""
+
+from .automation import BrowserAutomationSession
+
+__all__ = ["BrowserAutomationSession"]
